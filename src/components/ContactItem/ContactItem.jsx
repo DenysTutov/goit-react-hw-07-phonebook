41127421268fe01ae1c45e99e8a9ac860c9ec848
DenysTutov <93935076+DenysTutov@users.noreply.github.com>
2022-08-09 09:03:58 +0300

    Add global loading on delete button

--- src/components/ContactItem/ContactItem.jsx
+++ src/components/ContactItem/ContactItem.jsx
@@ -1,17 +1,16 @@
 import PropTypes from 'prop-types';
-import { useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import * as contactsOperation from 'redux/contactsOperation';
 import { Spinner } from '../Spinner/Spinner';
 import style from './ContactItem.module.scss';
-import { useState } from 'react';
+import { isDeleteLoading } from 'redux/contactsSlice';
+// import { useState } from 'react';
 
 const ContactItem = ({ id, name, number }) => {
   const dispatch = useDispatch();
-
-  const [loading, setLoading] = useState(false);
+  const loading = useSelector(isDeleteLoading);
 
   const handleDeleteContact = contactId => {
-    setLoading(true);
     dispatch(contactsOperation.deleteContact(contactId));
   };
 
@@ -38,3 +37,30 @@ ContactItem.propTypes = {
 };
 
 export default ContactItem;
+
+// Через локальный стейт
+// const ContactItem = ({ id, name, number }) => {
+//   const dispatch = useDispatch();
+
+//   const [loading, setLoading] = useState(false);
+
+//   const handleDeleteContact = contactId => {
+//     setLoading(true);
+//     dispatch(contactsOperation.deleteContact(contactId));
+//   };
+
+//   return (
+//     <li className={style.item}>
+//       <span className={style.itemName}>{name}:</span>
+//       <span className={style.itemNumber}>{number}</span>
+//       <button
+//         type="button"
+//         onClick={() => handleDeleteContact(id)}
+//         className={style.delete_btn}
+//         disabled={loading}
+//       >
+//         {!loading ? 'x' : <Spinner size={20} />}
+//       </button>
+//     </li>
+//   );
+// };
